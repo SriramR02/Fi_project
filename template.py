cc5786692663cefe8f6c6339521bae0b3a292002
SriramR02 <sriram2142002@gmail.com>
2025-01-30 20:34:46 +0530

    file strcture updted

--- template.py
+++ template.py
@@ -44,4 +44,3 @@ for filepath in list_of_files:
     if (not os.path.exists(filepath)) or(os.path.getsize(filepath) ==0):
         with open(filepath, "w") as f:
             pass #creating an empty file
->>>>>>> c1e6f57b9c6f9c8fbd2de92a413f9812bd3cd69d
